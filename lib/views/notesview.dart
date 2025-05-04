77ba319591eec9b88002cf0448f0c1de9067e16e
Bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-05-04 22:44:50 +0300

    making_custom_appbar

--- lib/views/notesview.dart
+++ lib/views/notesview.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/widgets/customappbar.dart';
 
 class Notesview extends StatelessWidget {
   const Notesview({super.key});
-  static const String id = 'notesview'  ;
+  static const String id = 'notesview';
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(children: [SizedBox(height: 26), Customappbar()]),
+    );
   }
 }
---
+++ lib/widgets/customappbar.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class Customappbar extends StatelessWidget {
+  const Customappbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      child: Row(
+        children: [
+          Text('Notes', style: TextStyle(fontSize: 20)),
+          Spacer(),
+          Container(
+            height: 32,
+            width: 32,
+            child: Icon(Icons.search),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              color: Colors.white.withAlpha((0.08 * 255).toInt()),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

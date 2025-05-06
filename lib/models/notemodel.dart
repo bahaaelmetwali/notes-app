bd67ed882320c686892cefa49539d853d14a8aea
Bahaaelmetwali <elmetwalibahaa@gmail.com>
2025-05-06 12:49:04 +0300

    creates_note-model

---
+++ lib/models/notemodel.dart
@@ -0,0 +1,12 @@
+class Notemodel {
+  final String title;
+  final String content;
+  final String date;
+  final int color;
+  Notemodel({
+    required this.title,
+    required this.content,
+    required this.color,
+    required this.date,
+  });
+}
